cebecefff10dbb603c0f2be1cf60511c10ff652a
leo-chok <stalh.leo@gmail.com>
2025-03-27 19:40:22 +0100

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const connectionString =
-  "mongodb+srv://admin:702RuSsDniWVQh6K@cluster0.9owmt.mongodb.net/tamagotchi";
+const connectionString = CONNECTION_STRING;
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))

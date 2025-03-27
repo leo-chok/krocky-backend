215ae300058c14cc5dfd8cc85dca6967e1e68d58
leochok <stalh.leo@gmail.com>
2025-03-27 18:22:03 +0100

    fix api cron

--- api/cron.js
+++ api/cron.js
@@ -1,6 +1,16 @@
 import connection from '../models/connection'
 import Monster from "../../models/monster";
 
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ ok: true });
+}
+
+if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+  return res.status(401).end('Unauthorized');
+}
+
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Méthode non autorisée" });
